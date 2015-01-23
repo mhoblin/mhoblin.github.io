886b5f66656f8c9ed481869e309c3c13a9b388d5
mhoblin <monicahoblin@eagles.ewu.edu>
2015-01-23 14:20:59 -0800

    Playing with the logo placement

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ $('i').addClass('big icon');
 
 $(window).resize(function(){
 	if ($(window).width() <= 800){	
-		$('header.column.row img').removeClass('left floated').addClass('ten wide center aligned');
-		$('header.column.row').addClass('center aligned')}
-    
-   	else {$('header.column.row img').addClass('left floated').removeClass('ten wide center aligned')}	
+		$('header#corner img').addClass('hide');
+		$('header#large img').removeClass('hide')}
+
+	else {$('header#large img').addClass('hide');
+			$('header#corner img').removeClass('hide')}
 });
+
